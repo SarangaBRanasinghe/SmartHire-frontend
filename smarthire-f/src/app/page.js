682117fe5610bba0e0ada5688a3a80f7b94808c2
Saranga Ranasinghe <ranasingherpksb.22@uom.lk>
2026-02-03 13:46:53 +0530

    create core valuse page

--- smarthire-f/src/app/page.js
+++ smarthire-f/src/app/page.js
@@ -5,6 +5,8 @@ import HowItWorks from "@/components/home/HowItWorks";
 import GetStarted from "@/components/home/GetStarted";
 import SucessStories from "@/components/home/SuccessStories";
 import Footer from "@/components/layout/Footer";
+import AboutSection from "@/components/home/AboutSection";
+import CoreValues from "@/components/home/CoreValues";
 
 export default function Home() {
   return (
@@ -16,6 +18,8 @@ export default function Home() {
         <HowItWorks />
         <GetStarted />
         <SucessStories />
+        <AboutSection/>
+        <CoreValues/>
       </main>
       <Footer />
     </>
